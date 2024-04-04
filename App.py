095556ac67d89caafec07786f3aedab8fd82bae6
KevinDaPlayer <325881@gmail.com>
2024-04-05 03:26:25 +0800

    ApiTestDebug

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
 import replicate
 import os
@@ -8,8 +9,9 @@ os.environ["REPLICATE_API_TOKEN"] = "r8_X5rkjHHRqccTNA4yJQhS5EKbEIsFBbT3Sdzge"
 
 @app.route('/predict_icd10',methods=['POST'])
 def predict_icd10():
-    prompt = request.json
-    if not prompt:
+    data = request.json  # 获取整个请求体的 JSON 字典
+    prompt_text = data.get('prompt',)
+    if not prompt_text:
         return jsonify("NO INPUT QUERY")
 
     output = replicate.run(
@@ -18,7 +20,7 @@ def predict_icd10():
             "debug": False,
             "top_k": 50,
             "top_p": 1,
-            "prompt": "病例描述" + prompt + "請基於這些信息，預測最可能的前五個 ICD-10 代碼。",
+            "prompt": "病例描述" + prompt_text + "請基於這些信息，預測最可能的前五個 ICD-10 代碼。",
             "temperature": 0.5,
             "system_prompt": "你是一位經驗豐富的醫師及疾病分類師，對 ICD-10 的分類系統非常熟悉。你的任務是根據所提供的病例描述，準確預測出最相關的前五個 ICD-10 代碼。你需要依據病例的症狀、病史及其他相關信息來做出判斷。請確保你的回答既精準又具有教育意義，幫助提升對 ICD-10 分類系統的理解。請在回答時列出前五個可能的 ICD-10 代碼，並簡要解釋每個代碼對應的疾病或病情。請以繁體中文回答，並且文本不要隨意換行",
             "max_new_tokens": 500,
@@ -33,4 +35,5 @@ def predict_icd10():
 
     return jsonify({accumulated_text})
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
